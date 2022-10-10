8095e28e57defa4e4374dd08970e2eff41bfd0e1
adt1710 <adrianoibira.adt@gmail.com>
2022-10-10 10:56:17 -0300

    Função de contraste finalizada

--- js/contraste.js
+++ js/contraste.js
@@ -1,2 +1,15 @@
 const btnContraste = document.querySelector("#modo_contraste");
 
+contrasteOn = false
+
+btnContraste.addEventListener('click', function () {
+    if (contrasteOn == false){
+        document.documentElement.style.setProperty('--primaria', '#1C1C1C');
+        document.documentElement.style.setProperty('--secundaria', '#363636');
+        document.documentElement.style.setProperty('--claro', 'black');
+        document.documentElement.style.setProperty('--escuro', 'white');
+        document.documentElement.style.setProperty('--botaoPri', 'white');
+        document.documentElement.style.setProperty('--botaoSec', 'gray');
+        document.documentElement.style.setProperty('--botaoText', 'black');
+    }
+})
